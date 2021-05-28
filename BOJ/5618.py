84117d5d12b0fc79db587dc50dfb88b66a34916a
kjjee99 <kjjee99@naver.com>
2021-05-28 23:01:40 +0900

    boj

---
+++ BOJ/5618.py
@@ -0,0 +1,10 @@
+#bj 5618**
+n = int(input())
+if n == 2:
+    li = list(map(int, input().split()))
+else:
+    li = list(map(int, input().split()))
+for a in li:
+    for i in range(1, max(li)):
+        if a % i == 0:
+            print(i)
