07bcd890c93f36aa281914ceab228a7bed20c8c4
kjjee99 <kjjee99@naver.com>
2021-06-23 22:27:53 +0900

    0623

---
+++ BOJ/Boj/src/bronze/BOJ_1085.java
@@ -0,0 +1,24 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1085 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+		StringTokenizer st =new StringTokenizer(br.readLine());
+		
+		int[] input = new int[4];
+		for(int i = 0; i < 4; i++) {
+			input[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int width = Math.min(input[2] - input[0], input[0]);
+		int height = Math.min(input[1], input[3] - input[1]);
+		System.out.println(Math.min(width, height));
+	}
+
+}
---
+++ BOJ/Boj/src/bronze/BOJ_2798.java
@@ -0,0 +1,40 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_2798 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+		StringTokenizer st =new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		st =new StringTokenizer(br.readLine());
+		int[] input = new int[n];
+		for(int i = 0; i < n; i++) {
+			input[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int result = 0, temp = 0;
+		for(int i = 0; i < n - 2; i++) {
+			if(input[i] > m) continue;
+			for(int j = i + 1; j < n - 1; j++) {
+				if(input[i] + input[j] > m) continue;
+				for(int k = j + 1; k < n; k++) {
+					temp = input[i] + input[j] + input[k];
+					if(temp == m) {
+						result = temp;
+						break;
+					}
+					if(result < temp && temp < m) result = temp;
+				}
+			}
+		}
+		System.out.println(result);
+	}
+
+}
