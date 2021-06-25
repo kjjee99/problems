1d3f74ce515b3de2dce8d5b229cca5d6a3ba1d24
kjjee99 <kjjee99@naver.com>
2021-06-26 00:03:47 +0900

    0626

---
+++ BOJ/Boj/src/bronze/BOJ_2775.java
@@ -0,0 +1,33 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2775 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int loop = Integer.parseInt(br.readLine());
+		int[][] list = new int[15][15];
+		
+		for(int i = 0; i < 15; i++) {
+			list[i][1] = 1;
+			list[0][i] = i;
+		}
+		for(int i = 1; i < 15; i++) {
+			for(int j = 2; j < 15; j++)
+				list[i][j] = list[i][j - 1] + list[i - 1][j];
+		}
+		while(loop > 0) {
+			int k = Integer.parseInt(br.readLine());
+			int n = Integer.parseInt(br.readLine());
+			
+			
+			System.out.println(list[k][n]);
+			loop--;
+		}
+	}
+
+}
