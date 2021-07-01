432f808565b8800de5871ec550a721482a6e0fdf
kjjee99 <kjjee99@naver.com>
2021-07-01 16:49:55 +0900

    update

--- DataStructure/src/list/CircleList.java
+++ DataStructure/src/list/CircleList.java
@@ -2,6 +2,34 @@ package list;
 
 public class CircleList {
 
+	private Node tail;
+	private int size = 0;
+	
+	public class Node{
+		private Object data;
+		private Node next;
+		public Node(Object input) {
+			this.data = input;
+			this.next = null;
+		}
+		
+		public String toString() {
+			return String.valueOf(this.data);
+		}
+	}
+	
+	public void inserLastNode(Object input) {
+		Node newNode = new Node(input);
+		if(tail == null) {
+			tail = newNode;
+			newNode.next = newNode;
+		}
+		else {
+			tail.next = newNode;
+		}
+		tail = newNode;
+		size++;   
+	}
 	public static void main(String[] args) {
 
 	}
