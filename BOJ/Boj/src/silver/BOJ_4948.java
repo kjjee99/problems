5b3aeda9577117bf953126695d8ef916a45ca045
kjjee99 <kjjee99@naver.com>
2021-12-23 21:55:23 +0900

    4948_소수구해서 범위 사이의 소수 구하기

--- BOJ/Boj/src/silver/BOJ_4948.java
+++ BOJ/Boj/src/silver/BOJ_4948.java
@@ -1,34 +1,39 @@
 package silver;
 
+//n<x<=2n의 소수 개수 찾기
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class BOJ_4948 {
 
 	public static void main(String[] args) throws IOException {
+		boolean[] prime = new boolean[246913];
+		prime[0] = prime[1] = true;
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		ArrayList<Boolean> prime = new ArrayList<Boolean>();
-		initial(prime);
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int cases = Integer.parseInt(br.readLine());
-		while(cases == 0){
-			int number = Integer.parseInt(br.readLine());
-			
-			for(int i = 2; i < number * 2; i++) {
-				if(prime.get(i)) {
-					for(int j = i * i; j < number * 2; j += i) {
-						prime.set(j, false);
-					}
+		//소수 미리 구하기
+		for (int i = 2; i < Math.sqrt(prime.length); i++) {
+			if (!prime[i]) {
+				for (int j = i * i; j < prime.length; j += i) {
+					prime[j] = true;
 				}
 			}
-			int result = prime.size();
+		}
+		int cases = Integer.parseInt(st.nextToken());
+		while (cases != 0) {
+			int result = 0;
+			for(int i = cases + 1; i < 2 * cases + 1; i++)	
+				if(!prime[i])	result++;
 			System.out.println(result);
-			cases--;
+			st = new StringTokenizer(br.readLine());
+			cases = Integer.parseInt(st.nextToken());
 		}
 	}
-	
+
 	public static void initial(ArrayList<Boolean> list) {
 		list.add(false);
 		list.add(false);
