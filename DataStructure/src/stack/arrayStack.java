084eda15dacd3de4a519b8f9a767680a89ea9836
kjjee99 <kjjee99@naver.com>
2022-01-07 20:41:04 +0900

    Array Stack(incomplete)

--- DataStructure/src/stack/arrayStack.java
+++ DataStructure/src/stack/arrayStack.java
@@ -11,21 +11,29 @@ public class arrayStack {
 		}
 	}
 	
-	public void sPush(int input) {
+	public void sPush(int input) {		//데이터 넣기
 		topIndex++;
 	}
 	
-	public void sPop(int input) {
+	public void sPop(int input) {		//마지막 데이터 추출 후 삭제
 		topIndex--;
 	}
 	
-	public int sPeek(int input) {
+	public int sPeek(int input) {		//마지막 데이터 보여주기
 		return stackArray[topIndex];
 	}
 	
-	public boolean sIsEmpty() {
+	public boolean sIsEmpty() {			//비어있는지 확인
 		if(topIndex == -1)		return true;
 		else					return false;
+	}
+	
+	public void clear() {				//스택 비우기
+		if(sIsEmpty()) System.out.println("Stack is already Empty");
+		else {
+			topIndex = -1;
+			stackArray = new int[100];
+		}
 	}
 	
 	public static void main(String[] args) {
