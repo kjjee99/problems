c8ca7d640893acbf72c275e2f4a39666337fd688
kjjee99 <kjjee99@naver.com>
2022-01-19 17:16:03 +0900

    complete Queue

---
+++ DataStructure/src/queue/TQueue.java
@@ -0,0 +1,65 @@
+package queue;
+
+public class TQueue {
+	private Node front = null;
+	private Node rear = null;
+	private int size = 0;
+
+	private class Node{
+		private Object data;
+		private Node next = null;
+		public Node(Object data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
+	
+	public void enqueue(Object input) {
+		Node node = new Node(input);
+		
+		
+		if(front == null) {
+			front = node;
+		}
+		else {
+			rear.next = node;
+		}
+		rear = node;
+		size++;
+		
+	}
+	
+	public void dequeue() {
+		Node temp = front.next;
+		front = temp;
+		if(size == 1) 	front = null;
+		size--;
+		
+	}
+	
+	public void show() {
+		Node x = front;
+		if(size == 0)	System.out.println("Queue is Empty");
+		for(int i = 0; i < size; i++) {
+			System.out.print(x.data+" ");
+			x = x.next;
+		}
+		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		TQueue queue = new TQueue();
+		queue.enqueue(1);
+		queue.enqueue(2);
+		queue.enqueue(3);
+		queue.show();
+		
+		queue.dequeue();
+		queue.dequeue();
+		queue.show();
+		
+		queue.dequeue();
+		queue.show();
+	}
+
+}
