bc4b247af7c2674f632decfce84fc7f623302bb0
kjjee99 <kjjee99@naver.com>
2022-02-18 14:16:51 +0900

    1427_역순정렬 추가

--- BOJ/Boj/src/silver/BOJ_1427.java
+++ BOJ/Boj/src/silver/BOJ_1427.java
@@ -17,6 +17,7 @@ public class BOJ_1427 {
 		
 		char[] arr = br.readLine().toCharArray();
 		Arrays.sort(arr);
+		//Arrays.sort(arr, Collections.reversOrder());
 		
 		for(int i = arr.length - 1; i >= 0; i--) {
 			System.out.print(arr[i]);
