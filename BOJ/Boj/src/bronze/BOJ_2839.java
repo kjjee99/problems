4867ed925b41cd6fec4dee74d05de9b0c9a71004
kjjee99 <kjjee99@naver.com>
2022-02-22 22:22:41 +0900

    2839_설탕배달_dp추가

--- BOJ/Boj/src/bronze/BOJ_2839.java
+++ BOJ/Boj/src/bronze/BOJ_2839.java
@@ -5,12 +5,13 @@ package bronze;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class BOJ_2839 {
 	public static BufferedReader sc = new BufferedReader (new InputStreamReader(System.in));
 	
 	//greedy
-	public static void main(String[] args) throws IOException{
+	/*public static void main(String[] args) throws IOException{
 		int sugar = Integer.parseInt(sc.readLine());
 		int bag = 0;
 		while (sugar >= 0) {
@@ -23,6 +24,29 @@ public class BOJ_2839 {
 		}
 		if(sugar < 0) 	bag = -1;
 		System.out.println(bag);
+	}*/
+	
+	//dp
+	public static void main(String[] args) throws IOException{
+		int sugar = Integer.parseInt(sc.readLine());
+		if(sugar < 5) {
+			if(sugar == 3)		System.out.println(1);
+			else				System.out.println(-1);
+			return;
+		}
+		
+		int dp[] = new int[sugar + 1];
+		Arrays.fill(dp, 9999);
+		dp[3] = 1;
+		dp[5] = 1;
+		for(int i = 6; i < dp.length; i++) {
+			dp[i] = Math.min(dp[i - 3] + 1, dp[i - 5] + 1);
+		}
+		if(dp[sugar] > 9999) {
+			System.out.println(-1);
+		}else {
+			System.out.println(dp[sugar]);
+		}
 	}
 
 	/*public static void main(String[] args) throws NumberFormatException, IOException {
