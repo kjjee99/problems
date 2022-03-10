c0d4b406421c061bfed47c3a98b81daa3e2c0ef2
kjjee99 <kjjee99@naver.com>
2022-03-10 21:07:34 +0900

    15652_백트래킹

---
+++ BOJ/Boj/src/silver/BOJ_15652.java
@@ -0,0 +1,42 @@
+package silver;
+
+/*
+ * N과 M(4)
+ * 백트래킹
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_15652 {
+	private static int arr[];
+	private static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		arr = new int[M];
+
+		dfs(1, N, M, 0);
+		System.out.println(sb);
+	}
+
+	private static void dfs(int at, int n, int m, int depth) {
+		if (depth == m) {
+			for (int value : arr)
+				sb.append(value).append(' ');
+			sb.append('\n');
+			return;
+		}
+		for (int i = at; i <= n; i++) {
+			arr[depth] = i;
+			dfs(i, n, m, depth + 1);
+
+		}
+	}
+
+}
