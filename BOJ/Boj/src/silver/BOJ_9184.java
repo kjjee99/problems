08115d3d866a73b093d46d68f066d5b310a8c0c6
kjjee99 <kjjee99@naver.com>
2022-03-15 17:16:33 +0900

    9184_재귀

---
+++ BOJ/Boj/src/silver/BOJ_9184.java
@@ -0,0 +1,40 @@
+package silver;
+
+/*
+ * 함수 어쩌구
+ * dp
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_9184 {
+	private static int result = 0;
+	private static int[][][] dp = new int[21][21][21];
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		while(true) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			if(a == -1 && b == -1 && c == -1)	return;
+			result = recur(a, b, c);
+			System.out.println("w("+a+", "+b+", "+c+") = "+result);
+		}
+	}
+	
+	private static int recur(int a, int b, int c) {
+		if(0 <= a && a <= 20 && 0 <= b && b <= 20 && 0<=c && c <= 20 && dp[a][b][c] != 0)
+			return dp[a][b][c];
+		if(a <= 0 || b <= 0 || c <= 0)			return 1;
+		else if(a > 20 || b > 20 || c > 20)		return dp[20][20][20] = recur(20, 20, 20);
+		else if(a < b && b < c)					return dp[a][b][c] = recur(a, b, c - 1) + recur(a, b - 1, c - 1) - recur(a, b - 1, c);
+		else									return dp[a][b][c] = recur(a - 1, b, c) + recur(a - 1, b - 1, c) + recur(a - 1, b, c -1) - recur(a - 1, b - 1, c - 1);
+	}	
+
+}
