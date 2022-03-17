f253da63614bd526d179b083aacc1886aa38f37d
kjjee99 <kjjee99@naver.com>
2022-03-17 16:08:48 +0900

    11279_최대힙

---
+++ BOJ/Boj/src/silver/BOJ_11279.java
@@ -0,0 +1,28 @@
+package silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class BOJ_11279 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+		while(N--> 0) {
+			int input = Integer.parseInt(br.readLine());
+			if(input == 0) {
+				if(queue.isEmpty())	System.out.println(0);
+				else				System.out.println(queue.poll());
+				continue;
+			}
+			queue.add(input);
+			
+		}
+	}
+
+}
