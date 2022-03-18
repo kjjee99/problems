8aad6c0b81cf9100ef942f662e5100b3b3ba64c7
kjjee99 <kjjee99@naver.com>
2022-03-18 23:16:44 +0900

    fix 11729

--- BOJ/Boj/src/silver/BOJ_11729.java
+++ BOJ/Boj/src/silver/BOJ_11729.java
@@ -7,10 +7,8 @@ package silver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class BOJ_11729 {
-	private static ArrayList<int[]> list = new ArrayList<>();
 	private static StringBuilder sb = new StringBuilder();
 
 	public static void main(String[] args) throws IOException{
