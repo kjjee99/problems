6c5381cc86d7a75b435b8939237d382b82ca2093
kjjee99 <kjjee99@naver.com>
2022-03-18 23:16:30 +0900

    11729_하노이

---
+++ BOJ/Boj/src/silver/BOJ_11729.java
@@ -0,0 +1,36 @@
+package silver;
+
+/*
+ * Hanoi Tower
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class BOJ_11729 {
+	private static ArrayList<int[]> list = new ArrayList<>();
+	private static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		int count = (int) (Math.pow(2, N) - 1);
+		sb.append(count).append('\n');
+		hanoi(N, 1, 2, 3);
+		System.out.println(sb);
+	}
+
+	private static void hanoi(int number, int from, int by, int to) {
+		if(number == 1) {
+			sb.append(from+" "+to).append('\n');
+			return;
+		}
+		
+		hanoi(number - 1, from, to, by);
+		sb.append(from+" "+to).append('\n');
+		hanoi(number - 1, by, from, to);
+	}
+}
